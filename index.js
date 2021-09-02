3303c6ab3c0dfe85d47ca9b07d4d8cd2062d8422
Broveku <gearhearjordan@gmail.com>
2021-09-02 10:08:17 -0700

    done for now

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 
-console.log(process.env.USER)
-console.log(process.env.HOME)
-console.log(process.env.PORT)
+
+console.log(process.env.API_SECRET)
 
 const PORT = process.env.PORT || 5000
+
 
 const server = express()
 
